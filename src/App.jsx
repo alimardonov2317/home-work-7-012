0a5c8e3306df0ba0f2a97223f5209b56288c6ac8
alimardonov2317 <alimardonovsamandar707@gmail.com>
2025-03-01 17:18:56 +0500

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-import Header from "./components/Header";
-import Products from "./components/products";
 import Hero from "./components/Hero";
 
 
 const App = () => {
   return (
     <div>
-      {/* <Header title="Header section" year={2025}/> */}
       <Hero />
-      {/* <Products/> */}
     </div>
   );
 };
